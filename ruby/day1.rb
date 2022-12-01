416641a5c3ec7814b736e58c79ba006df96def4f
Aidan Wolk <aidanwolk@comcast.net>
2022-11-30 21:09:41 -0800

    Implement day 1

---
+++ ruby/day1.rb
@@ -0,0 +1,10 @@
+require_relative './aoc'
+
+calories_per_elf = AOC.get_input(1).split("\n\n").map do |elf|
+  elf.split.sum(&:to_i)
+end.sort
+pt1 = calories_per_elf.last
+puts "Part 1: #{pt1}"
+
+pt2 = calories_per_elf.last(3).sum
+puts "Part 2: #{pt2}"
