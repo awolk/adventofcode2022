122de35af78cb2c51369a800d53c5d45884f3c7e
Aidan Wolk <aidanwolk@comcast.net>
2022-12-20 00:10:48 -0800

    Speed up day 20

--- ruby/day20.rb
+++ ruby/day20.rb
@@ -2,31 +2,33 @@ require 'set'
 require_relative './lib/aoc'
 require_relative './lib/parser'
 
-def mix(values, order)
-  order.each_with_index do |val|
-    index = values.index(val)
-    values.rotate!(index) # put val first
-    values.shift
-    values.insert(val[0] % values.length, val)
+def mix!(indices, original)
+  indices.length.times do |i|
+    index = indices.index(i)
+    indices.delete_at(index)
+    indices.insert((index + original[i]) % indices.length, i)
   end
 end
 
-def sum_grove(values)
-  zero_index = values.find_index {|int, _| int == 0}
-  [1000, 2000, 3000].sum {|ind| values[(zero_index + ind) % values.length][0]}
+def sum_grove(indices, original)
+  original_zero_index = original.index(0)
+  zero_index = indices.index(original_zero_index)
+  [1000, 2000, 3000].sum do |ind|
+    index_into_orig = indices[(zero_index + ind) % indices.length]
+    original[index_into_orig]
+  end
 end
 
-input = P.int.each_line.parse_all(AOC.get_input(20)).each_with_index.to_a
-
 # Part 1
-res = input.clone
-mix(res, input)
-pt1 = sum_grove(res)
+original = P.int.each_line.parse_all(AOC.get_input(20))
+indices = (0...original.length).to_a
+mix!(indices, original)
+pt1 = sum_grove(indices, original)
 puts "Part 1: #{pt1}"
 
 # Part 2
-input = input.map {|val, ind| [val * 811589153, ind]}
-res = input.clone
-10.times {mix(res, input)}
-pt2 = sum_grove(res)
+pt2_orig = original.map {|i| i * 811589153}
+indices = (0...pt2_orig.length).to_a
+10.times {mix!(indices, pt2_orig)}
+pt2 = sum_grove(indices, pt2_orig)
 puts "Part 2: #{pt2}"
